da13be55b615a02330dcf80cdd4e7755eacee294
jeeva <48005645+jeevajo@users.noreply.github.com>
2025-12-06 22:08:01 +0530

    Add files via upload

---
+++ src/data/products.js
@@ -0,0 +1,125 @@
+const products = [
+  {
+    id: 'kyarr-30w-33',
+    title: 'KYARR 30W Solar Street Light (30W33)',
+    panel: '80W Solar Panel',
+    battery: 'LiFePO4 12.8V / 30AH',
+    led: '30W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-1.png'
+  },
+  {
+    id: 'kyarr-20w-33',
+    title: 'KYARR 20W Solar Street Light (20W33)',
+    panel: '60W Solar Panel',
+    battery: 'LiFePO4 12.8V / 24AH',
+    led: '20W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-2.png'
+  },
+  {
+    id: 'kyarr-40w-33',
+    title: 'KYARR 40W Solar Street Light (40W33)',
+    panel: '100W Solar Panel',
+    battery: 'LiFePO4 12.8V / 36AH',
+    led: '40W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-3.png'
+  },
+  {
+    id: 'kyarr-80w-33',
+    title: 'KYARR 80W Solar Street Light (80W33)',
+    panel: '180W Solar Panel',
+    battery: 'LiFePO4 12.8V / 80AH',
+    led: '80W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-4.png'
+  },
+  {
+    id: 'kyarr-double-40w',
+    title: 'KYARR 40W + 40W Double-Arm Solar Street Light',
+    panel: '180W Solar Panel',
+    battery: 'LiFePO4 12.8V / 80AH',
+    led: '40W + 40W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-5.png'
+  },
+  {
+    id: 'kyarr-30w-2in1',
+    title: 'KYARR 30W 2-in-1 Solar Street Light',
+    panel: '80W Solar Panel',
+    battery: 'LiFePO4 12.8V / 30AH',
+    led: '30W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-6.png'
+  },
+  {
+    id: 'kyarr-20w-2in1',
+    title: 'KYARR 20W 2-in-1 Solar Street Light',
+    panel: '60W Solar Panel',
+    battery: 'LiFePO4 12.8V / 24AH',
+    led: '20W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-7.png'
+  },
+  {
+    id: 'kyarr-40w-2in1',
+    title: 'KYARR 40W 2-in-1 Solar Street Light',
+    panel: '100W Solar Panel',
+    battery: 'LiFePO4 12.8V / 36AH',
+    led: '40W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-8.png'
+  },
+  {
+    id: 'kyarr-40w-aio',
+    title: 'KYARR 40W All-in-One Solar Street Light',
+    panel: '100W Solar Panel',
+    battery: 'LiFePO4 12.8V / 36AH',
+    led: '40W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-9.png'
+  },
+  {
+    id: 'kyarr-80w-aio',
+    title: 'KYARR 80W All-in-One Solar Street Light',
+    panel: '180W Solar Panel',
+    battery: 'LiFePO4 12.8V / 80AH',
+    led: '80W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-10.png'
+  },
+  {
+    id: 'kyarr-100w-aio',
+    title: 'KYARR 100W All-in-One Solar Street Light',
+    panel: '200W Solar Panel',
+    battery: 'LiFePO4 12.8V / 100AH',
+    led: '100W',
+    backup: '2 Nights',
+    warranty: '3 Years',
+    serviceWarranty: '2 Years',
+    image: '/images/product-11.png'
+  }
+];
+
+export default products;
